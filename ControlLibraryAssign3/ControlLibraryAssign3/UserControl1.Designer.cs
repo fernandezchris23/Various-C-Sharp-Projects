fee99acb84a644867065aebcf89d7f26cc6f1da7
Vanesa <vpere159@fiu.edu>
2018-09-15 21:07:24 -0400

    User Control, attempting to save solution to my branch

---
+++ ControlLibraryAssign3/ControlLibraryAssign3/UserControl1.Designer.cs
@@ -0,0 +1,142 @@
+﻿namespace ControlLibraryAssign3
+{
+    partial class UserControl1
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupNameLabel = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.memberNameLabel1 = new System.Windows.Forms.Label();
+            this.memberNameLabel2 = new System.Windows.Forms.Label();
+            this.memberNameLabel3 = new System.Windows.Forms.Label();
+            this.memberNameLabel4 = new System.Windows.Forms.Label();
+            this.memberNameLabel5 = new System.Windows.Forms.Label();
+            this.memberNameLabel6 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // groupNameLabel
+            // 
+            this.groupNameLabel.AutoSize = true;
+            this.groupNameLabel.Location = new System.Drawing.Point(0, 0);
+            this.groupNameLabel.Name = "groupNameLabel";
+            this.groupNameLabel.Size = new System.Drawing.Size(138, 17);
+            this.groupNameLabel.TabIndex = 0;
+            this.groupNameLabel.Text = "Team 2: We\'re Okay";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(0, 17);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(121, 17);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Alejandro Barnola";
+            // 
+            // memberNameLabel1
+            // 
+            this.memberNameLabel1.AutoSize = true;
+            this.memberNameLabel1.Location = new System.Drawing.Point(0, 17);
+            this.memberNameLabel1.Name = "memberNameLabel1";
+            this.memberNameLabel1.Size = new System.Drawing.Size(0, 17);
+            this.memberNameLabel1.TabIndex = 0;
+            // 
+            // memberNameLabel2
+            // 
+            this.memberNameLabel2.AutoSize = true;
+            this.memberNameLabel2.Location = new System.Drawing.Point(0, 34);
+            this.memberNameLabel2.Name = "memberNameLabel2";
+            this.memberNameLabel2.Size = new System.Drawing.Size(88, 17);
+            this.memberNameLabel2.TabIndex = 0;
+            this.memberNameLabel2.Text = "Walter Elwell";
+            // 
+            // memberNameLabel3
+            // 
+            this.memberNameLabel3.AutoSize = true;
+            this.memberNameLabel3.Location = new System.Drawing.Point(0, 51);
+            this.memberNameLabel3.Name = "memberNameLabel3";
+            this.memberNameLabel3.Size = new System.Drawing.Size(153, 17);
+            this.memberNameLabel3.TabIndex = 0;
+            this.memberNameLabel3.Text = "Christopher Fernandez";
+            // 
+            // memberNameLabel4
+            // 
+            this.memberNameLabel4.AutoSize = true;
+            this.memberNameLabel4.Location = new System.Drawing.Point(0, 68);
+            this.memberNameLabel4.Name = "memberNameLabel4";
+            this.memberNameLabel4.Size = new System.Drawing.Size(98, 17);
+            this.memberNameLabel4.TabIndex = 0;
+            this.memberNameLabel4.Text = "Alvaro Orozco";
+            // 
+            // memberNameLabel5
+            // 
+            this.memberNameLabel5.AutoSize = true;
+            this.memberNameLabel5.Location = new System.Drawing.Point(0, 85);
+            this.memberNameLabel5.Name = "memberNameLabel5";
+            this.memberNameLabel5.Size = new System.Drawing.Size(77, 17);
+            this.memberNameLabel5.TabIndex = 0;
+            this.memberNameLabel5.Text = "Kayla Ortiz";
+            // 
+            // memberNameLabel6
+            // 
+            this.memberNameLabel6.AutoSize = true;
+            this.memberNameLabel6.Location = new System.Drawing.Point(0, 102);
+            this.memberNameLabel6.Name = "memberNameLabel6";
+            this.memberNameLabel6.Size = new System.Drawing.Size(97, 17);
+            this.memberNameLabel6.TabIndex = 0;
+            this.memberNameLabel6.Text = "Vanesa Perez";
+            // 
+            // UserControl1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.memberNameLabel6);
+            this.Controls.Add(this.memberNameLabel5);
+            this.Controls.Add(this.memberNameLabel4);
+            this.Controls.Add(this.memberNameLabel3);
+            this.Controls.Add(this.memberNameLabel2);
+            this.Controls.Add(this.memberNameLabel1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.groupNameLabel);
+            this.Name = "UserControl1";
+            this.Size = new System.Drawing.Size(212, 188);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label groupNameLabel;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label memberNameLabel1;
+        private System.Windows.Forms.Label memberNameLabel2;
+        private System.Windows.Forms.Label memberNameLabel3;
+        private System.Windows.Forms.Label memberNameLabel4;
+        private System.Windows.Forms.Label memberNameLabel5;
+        private System.Windows.Forms.Label memberNameLabel6;
+    }
+}
