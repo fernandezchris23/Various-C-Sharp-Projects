3e8051f32f65f11b5eb1dca6716a45816e4e19f5
korti029 <42555193+korti029@users.noreply.github.com>
2018-09-17 10:53:45 -0400

    Add files via upload
    Interfaces and Shapes so far. Where to put everything needs work but here are the methods for now

---
+++ Ellipse.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MainFormFiles
+{
+    public partial class Ellipse : Form
+    {
+        public Ellipse(double multiplier, int width)
+        {
+            this.width = width;
+            this.height = (int)(width * multiplier);
+
+            InitializeComponent();
+            this.ClientSize = new System.Drawing.Size(width, height);
+            this.FormBorderStyle = FormBorderStyle.None;
+            this.BackColor = Color.Aqua;
+            
+            SetEllipseRegion();
+        }
+        void SetEllipseRegion()
+        {
+
+            Rectangle rect = this.ClientRectangle;
+
+            using (GraphicsPath path = new GraphicsPath())
+            {
+                path.AddEllipse(rect);
+                this.Region = new Region(path);
+            }
+
+        }
+
+        void Ellipse_Load(object sender, EventArgs e)
+        {
+            SetEllipseRegion();
+        }
+        void Ellipse_SizeChanged(object sender, EventArgs e)
+        {
+            SetEllipseRegion();
+        }
+
+        void Ellipse_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (e.Button != MouseButtons.Left) return;
+
+            downPoint = new Point(e.X, e.Y);
+
+        }
+
+        void MainForm_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (downPoint == Point.Empty)
+                return;
+
+            Point location = new Point(this.Left + e.X - downPoint.Y,
+                    this.Top + e.Y - downPoint.Y);
+        }
+
+        void Ellipse_MouseUp(object sender, MouseEventArgs e)
+        {
+            if (e.Button != MouseButtons.Left)
+                return;
+
+            downPoint = Point.Empty;
+        }
+
+        Point downPoint = Point.Empty;
+        private int height;
+        private int width;
+    }
+}
---
+++ Rectangular.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Assignment3;
+
+namespace MainFormFiles
+{
+    public partial class Rectangular : BaseForm
+    {
+        public Rectangular(int height,double multiple)
+        {
+            this.height = height;
+            this.width = (int) multiple * height;
+
+            InitializeComponent();
+            this.ClientSize = new System.Drawing.Size((int)multiple*height,
+                height);
+            this.FormBorderStyle = FormBorderStyle.None;
+            this.BackColor = Color.Gold;
+        }
+
+        void SetRectangleRegion()
+        {
+            using (GraphicsPath path = new GraphicsPath())
+            {
+                path.AddRectangle(this.ClientRectangle);
+                this.Region = new Region(path);
+            }
+        }
+
+        private int height;
+        private int width;
+    }
+}
