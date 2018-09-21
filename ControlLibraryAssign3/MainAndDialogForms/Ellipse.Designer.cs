6ccae05ce27f0e976958bce42f93d05a481edbd1
Alejandro Barnola <Alebarnola96@gmail.com>
2018-09-21 14:14:39 -0400

    Assignment3-A Video Version

---
+++ ControlLibraryAssign3/MainAndDialogForms/Ellipse.Designer.cs
@@ -0,0 +1,51 @@
+﻿namespace MainAndDialogForms
+{
+    partial class Ellipse
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // Ellipse
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1067, 554);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Margin = new System.Windows.Forms.Padding(4, 2, 4, 2);
+            this.Name = "Ellipse";
+            this.Text = "Ellipse";
+            this.TopMost = true;
+            this.Load += new System.EventHandler(this.Ellipse_Load);
+            this.SizeChanged += new System.EventHandler(this.Ellipse_SizeChanged);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
