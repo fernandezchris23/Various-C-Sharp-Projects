97e22fcb14a959fc85ca7a9b1c0b71b54e460a2d
Alejandro Barnola <Alebarnola96@gmail.com>
2018-09-21 14:13:52 -0400

    Assign3-A

---
+++ Assignment 3-A/Ellipse.cs
@@ -0,0 +1,52 @@
+    using System;
+    using System.Drawing;
+    using System.Windows.Forms;
+
+namespace Assignment3A
+{
+	partial class Ellipse : Form
+    {
+		public Ellipse (int width, double height_multiplier)
+        {
+            this.Width = width;
+            this.Height = (int)(width * height_multiplier);
+
+            InitializeComponent();
+
+            this.ClientSize = new Size(this.Width, this.Height);
+            this.FormBorderStyle = FormBorderStyle.None;
+            this.BackColor = System.Drawing.Color.Red;
+
+            SetEllipseRegion();
+
+        }
+
+        void SetEllipseRegion()
+        {
+            Rectangle rectangle = this.ClientRectangle;
+
+            using (GraphicsPath path = new GraphicsPath())
+            {
+                path.AddEllipse(rectangle);
+                this.Region = new Region(path);
+            }
+        }
+
+        void LoadEllipse(object sender, EventArgs e)
+        {
+            SetEllipseRegion();
+        }
+
+        void EllipseSizeChanged(object sender, EventArgs e)
+        {
+            SetEllipseRegion();
+        }
+
+        /*  Not how the book does this, seemed interesting and fairly simple so I left it here
+		void DrawEllipse()
+        {
+            
+        }
+        */
+    }
+}
---
+++ WindowsFormsApp1/WindowsFormsApp1/Ellipse.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    internal class Ellipse : Form
+    {
+ 
+        public Ellipse(int width, double height_multiplier)
+        {
+            this.Width = width;
+            this.Height = (int) (width * height_multiplier);
+
+            
+
+        }
+    }
+}
