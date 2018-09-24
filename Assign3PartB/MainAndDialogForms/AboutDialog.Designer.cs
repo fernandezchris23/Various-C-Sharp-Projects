eb129b348fad6ed04042bee87fd1cc3a16f4df1f
Vanesa <vpere159@fiu.edu>
2018-09-24 15:55:23 -0400

    About dialog fixed?

--- Assign3PartB/MainAndDialogForms/AboutDialog.Designer.cs
+++ Assign3PartB/MainAndDialogForms/AboutDialog.Designer.cs
@@ -35,8 +35,8 @@
             // dialogBaseFormPanel
             // 
             this.dialogBaseFormPanel.Controls.Add(this.assignDescrip);
-            this.dialogBaseFormPanel.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
-            this.dialogBaseFormPanel.Size = new System.Drawing.Size(1037, 224);
+            this.dialogBaseFormPanel.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.dialogBaseFormPanel.Size = new System.Drawing.Size(778, 182);
             // 
             // assignDescrip
             // 
@@ -46,7 +46,7 @@
             this.assignDescrip.AutoSize = true;
             this.assignDescrip.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.assignDescrip.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(64)))));
-            this.assignDescrip.Location = new System.Drawing.Point(4, 69);
+            this.assignDescrip.Location = new System.Drawing.Point(3, 56);
             this.assignDescrip.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.assignDescrip.Name = "assignDescrip";
             this.assignDescrip.Size = new System.Drawing.Size(981, 87);
