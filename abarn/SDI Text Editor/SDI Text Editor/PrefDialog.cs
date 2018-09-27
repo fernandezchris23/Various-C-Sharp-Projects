e081902d9f402ac798d3fde05e32a5fd2c05b0af
Alejandro Barnola <Alebarnola96@gmail.com>
2018-09-27 18:17:52 -0400

    Created main form, merged prefs dialog from last week

---
+++ abarn/SDI Text Editor/SDI Text Editor/PrefDialog.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SDI_Text_Editor
+{
+    public partial class PrefDialog : Form
+    {   
+        private int RectHeight;
+        private int EllipWidth;
+        private float Ratio;
+        private MainForm mainForm;
+        
+        public PrefDialog()
+        {
+            InitializeComponent();
+            RectHeight = 0;
+            Ratio = 0f;
+            EllipWidth = 0;
+            mainForm = null;
+        }
+
+        public PrefDialog(MainForm mainForm)
+        {
+            InitializeComponent();
+            this.mainForm = mainForm;
+            RectHeight = 0;
+            Ratio = 0f;
+            EllipWidth = 0;
+        }
+
+        private void PrefDialog_Load(object sender, EventArgs e)
+        {
+        }        
+
+        private void okayButton_Click(object sender, EventArgs e)
+        {            
+            this.Close();
+        }
+
+        private void applyButton_Click(object sender, EventArgs e)
+        {
+                        
+        }
+
+        private void cancelButton_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
