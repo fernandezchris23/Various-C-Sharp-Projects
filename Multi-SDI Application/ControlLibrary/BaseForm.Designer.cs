20ac8a8e89376a23b9a6ed423d484d39628613d1
Alejandro Barnola <Alebarnola96@gmail.com>
2018-10-09 01:35:35 -0400

    just created the project... added Help and Help/Oath, Help/About buttons to toolstrip on BaseForm from ControlLibrary

---
+++ Multi-SDI Application/ControlLibrary/BaseForm.Designer.cs
@@ -0,0 +1,156 @@
+﻿namespace ControlLibraryAssign3
+{
+    partial class BaseForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(BaseForm));
+            this.baseFormsContextMenuStrip = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.closeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.preferencesToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.colorsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.baseFormColorDialog = new System.Windows.Forms.ColorDialog();
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.toolStripDropDownButton1 = new System.Windows.Forms.ToolStripDropDownButton();
+            this.aboutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.oathToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.baseFormsContextMenuStrip.SuspendLayout();
+            this.toolStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // baseFormsContextMenuStrip
+            // 
+            this.baseFormsContextMenuStrip.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.baseFormsContextMenuStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.preferencesToolStripMenuItem});
+            this.baseFormsContextMenuStrip.Name = "baseFormsContextMenuStrip";
+            this.baseFormsContextMenuStrip.Size = new System.Drawing.Size(155, 52);
+            this.baseFormsContextMenuStrip.Text = "Menu";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.closeToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(154, 24);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // closeToolStripMenuItem
+            // 
+            this.closeToolStripMenuItem.Name = "closeToolStripMenuItem";
+            this.closeToolStripMenuItem.Size = new System.Drawing.Size(120, 26);
+            this.closeToolStripMenuItem.Text = "Close";
+            this.closeToolStripMenuItem.Click += new System.EventHandler(this.closeToolStripMenuItem_Click);
+            // 
+            // preferencesToolStripMenuItem
+            // 
+            this.preferencesToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.colorsToolStripMenuItem});
+            this.preferencesToolStripMenuItem.Name = "preferencesToolStripMenuItem";
+            this.preferencesToolStripMenuItem.Size = new System.Drawing.Size(154, 24);
+            this.preferencesToolStripMenuItem.Text = "Preferences";
+            // 
+            // colorsToolStripMenuItem
+            // 
+            this.colorsToolStripMenuItem.Name = "colorsToolStripMenuItem";
+            this.colorsToolStripMenuItem.Size = new System.Drawing.Size(126, 26);
+            this.colorsToolStripMenuItem.Text = "Colors";
+            this.colorsToolStripMenuItem.Click += new System.EventHandler(this.colorsToolStripMenuItem_Click);
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripDropDownButton1});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 0);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(800, 27);
+            this.toolStrip1.TabIndex = 2;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // toolStripDropDownButton1
+            // 
+            this.toolStripDropDownButton1.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.toolStripDropDownButton1.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.aboutToolStripMenuItem,
+            this.oathToolStripMenuItem});
+            this.toolStripDropDownButton1.Image = ((System.Drawing.Image)(resources.GetObject("toolStripDropDownButton1.Image")));
+            this.toolStripDropDownButton1.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripDropDownButton1.Name = "toolStripDropDownButton1";
+            this.toolStripDropDownButton1.Size = new System.Drawing.Size(55, 24);
+            this.toolStripDropDownButton1.Text = "Help";
+            // 
+            // aboutToolStripMenuItem
+            // 
+            this.aboutToolStripMenuItem.Name = "aboutToolStripMenuItem";
+            this.aboutToolStripMenuItem.Size = new System.Drawing.Size(216, 26);
+            this.aboutToolStripMenuItem.Text = "About";
+            // 
+            // oathToolStripMenuItem
+            // 
+            this.oathToolStripMenuItem.Name = "oathToolStripMenuItem";
+            this.oathToolStripMenuItem.Size = new System.Drawing.Size(216, 26);
+            this.oathToolStripMenuItem.Text = "Oath";
+            // 
+            // BaseForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ContextMenuStrip = this.baseFormsContextMenuStrip;
+            this.Controls.Add(this.toolStrip1);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Name = "BaseForm";
+            this.Text = "BaseForm";
+            this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Base_MouseDown);
+            this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Base_MouseMove);
+            this.MouseUp += new System.Windows.Forms.MouseEventHandler(this.Base_MouseUp);
+            this.baseFormsContextMenuStrip.ResumeLayout(false);
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ContextMenuStrip baseFormsContextMenuStrip;
+        private System.Windows.Forms.ToolStripMenuItem preferencesToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem colorsToolStripMenuItem;
+        private System.Windows.Forms.ColorDialog baseFormColorDialog;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem closeToolStripMenuItem;
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripDropDownButton toolStripDropDownButton1;
+        private System.Windows.Forms.ToolStripMenuItem aboutToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem oathToolStripMenuItem;
+    }
+}
---
+++ Multi-SDI Application/ControlLibrary/DialogBaseForm.Designer.cs
@@ -0,0 +1,87 @@
+﻿namespace ControlLibraryAssign3
+{
+    partial class DialogBaseForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dialogBaseFormPanel = new System.Windows.Forms.Panel();
+            this.courseUserControl = new ControlLibraryAssign3.CourseUserControl();
+            this.namesUserControl1 = new ControlLibraryAssign3.NamesUserControl();
+            this.SuspendLayout();
+            // 
+            // dialogBaseFormPanel
+            // 
+            this.dialogBaseFormPanel.BackColor = System.Drawing.Color.Transparent;
+            this.dialogBaseFormPanel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dialogBaseFormPanel.Location = new System.Drawing.Point(0, 53);
+            this.dialogBaseFormPanel.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.dialogBaseFormPanel.Name = "dialogBaseFormPanel";
+            this.dialogBaseFormPanel.Size = new System.Drawing.Size(1115, 331);
+            this.dialogBaseFormPanel.TabIndex = 2;
+            // 
+            // courseUserControl
+            // 
+            this.courseUserControl.BackColor = System.Drawing.Color.Transparent;
+            this.courseUserControl.Dock = System.Windows.Forms.DockStyle.Top;
+            this.courseUserControl.Location = new System.Drawing.Point(0, 0);
+            this.courseUserControl.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.courseUserControl.Name = "courseUserControl";
+            this.courseUserControl.Size = new System.Drawing.Size(1115, 53);
+            this.courseUserControl.TabIndex = 1;
+            // 
+            // namesUserControl1
+            // 
+            this.namesUserControl1.BackColor = System.Drawing.Color.LightGray;
+            this.namesUserControl1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.namesUserControl1.Location = new System.Drawing.Point(0, 384);
+            this.namesUserControl1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.namesUserControl1.Name = "namesUserControl1";
+            this.namesUserControl1.Size = new System.Drawing.Size(1115, 64);
+            this.namesUserControl1.TabIndex = 0;
+            // 
+            // DialogBaseForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1115, 448);
+            this.Controls.Add(this.dialogBaseFormPanel);
+            this.Controls.Add(this.courseUserControl);
+            this.Controls.Add(this.namesUserControl1);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "DialogBaseForm";
+            this.Text = "DialogBaseForm";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private NamesUserControl namesUserControl1;
+        private CourseUserControl courseUserControl;
+        protected internal System.Windows.Forms.Panel dialogBaseFormPanel;
+    }
+}
