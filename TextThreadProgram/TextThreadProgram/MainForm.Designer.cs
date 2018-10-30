3edd3a612be936de470f91f1a201eb5867090513
Vanesa <vpere159@fiu.edu>
2018-10-30 16:08:00 -0400

    Chaning main form title to from default form 1

--- TextThreadProgram/TextThreadProgram/MainForm.Designer.cs
+++ TextThreadProgram/TextThreadProgram/MainForm.Designer.cs
@@ -169,7 +169,7 @@
             this.Controls.Add(this.menuStrip1);
             this.MainMenuStrip = this.menuStrip1;
             this.Name = "MainForm";
-            this.Text = "Form1";
+            this.Text = "Main Form";
             this.menuStrip1.ResumeLayout(false);
             this.menuStrip1.PerformLayout();
             this.statusStrip1.ResumeLayout(false);
