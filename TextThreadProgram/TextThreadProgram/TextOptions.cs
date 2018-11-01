83fb08e4ab81da37fe933229a068e6cf525c0bb9
Alejandro Barnola <Alebarnola96@gmail.com>
2018-11-01 00:50:50 -0400

    add textoptions changes from alejandro

--- TextThreadProgram/TextThreadProgram/TextOptions.cs
+++ TextThreadProgram/TextThreadProgram/TextOptions.cs
@@ -13,8 +13,8 @@ namespace TextThreadProgram
 {
     public partial class TextOptions : BaseDialogForm
     {
-        Text inOptions;
-        Text outOptions;
+
+        Text textProperties;
         public TextOptions()
         {
             InitializeComponent();
@@ -22,71 +22,75 @@ namespace TextThreadProgram
 
         public TextOptions(Text text)
         {
-            inOptions = text;
-            outOptions = text;
+            textProperties = text;
             InitializeComponent();
         }
 
         private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
         {
-            switch (comboBox1.SelectedText)
+            propertyTextBox.ReadOnly = true;
+            switch (comboBox1.Text)
             {
                 case "Text":
-                    propertyTextBox.Font = outOptions.TextFont;
-                    propertyTextBox.Text = outOptions.StringText;
+                    propertyTextBox.Font = textProperties.TextFont;
+                    propertyTextBox.Text = textProperties.StringText;
+                    propertyTextBox.BackColor = Color.White;
                     break;
 
                 case "Font":
-                    propertyTextBox.Font = outOptions.TextFont;
-                    propertyTextBox.Text = outOptions.TextFont.ToString();
+                    propertyTextBox.Font = textProperties.TextFont;
+                    propertyTextBox.Text = textProperties.TextFont.FontFamily.Name + ", " + textProperties.TextFont.Size;
+                    propertyTextBox.BackColor = Color.White;
                     break;
 
                 case "Text Color":
-                    propertyTextBox.BackColor = outOptions.TextColor;
+                    propertyTextBox.BackColor = textProperties.TextColor;
+                    propertyTextBox.Text = "";
                     break;
 
                 case "Background Color":
-                    propertyTextBox.BackColor = outOptions.BgColor;
+                    propertyTextBox.BackColor = textProperties.BgColor;
+                    propertyTextBox.Text = "";
                     break;
 
                 case "Z-Order":
-                    if (outOptions.Z_Order == 0)
+                    propertyTextBox.BackColor = Color.White;
+                    if (textProperties.Z_Order == 0)
                         propertyTextBox.Text = "Back";
                     else
                         propertyTextBox.Text = "Front";
                     break;
 
                 case "Location":
-                    propertyTextBox.Text = outOptions.TextLocation.ToString();
+                    propertyTextBox.Text = textProperties.TextLocation.ToString();
+                    propertyTextBox.BackColor = Color.White;
                     break;
 
                 case "Rotation":
-                    propertyTextBox.Text = outOptions.Rotation.ToString();
+                    propertyTextBox.Text = textProperties.Rotation.ToString();
+                    propertyTextBox.BackColor = Color.White;
                     break;
             }
-                    
+
         }
 
         private void changeButton_Click(object sender, EventArgs e)
         {
-            switch (comboBox1.SelectedText)
+            switch (comboBox1.Text)
             {
                 case "Text Color":
                     if (colorDialog.ShowDialog() == DialogResult.OK)
-                        outOptions.TextColor = colorDialog.Color;
-                    propertyTextBox.BackColor = outOptions.TextColor;
+                        propertyTextBox.BackColor = colorDialog.Color;
                     break;
 
                 case "Background Color":
                     if (colorDialog.ShowDialog() == DialogResult.OK)
-                        outOptions.BgColor = colorDialog.Color;
-                    propertyTextBox.BackColor = outOptions.BgColor;
+                        propertyTextBox.BackColor = colorDialog.Color;
                     break;
 
                 case "Font":
                     if (fontDialog.ShowDialog() == DialogResult.OK)
-                        outOptions.TextFont = fontDialog.Font;
-                    propertyTextBox.Font = outOptions.TextFont;
+                        propertyTextBox.Font = fontDialog.Font;
                     break;
 
                 case "Text":
@@ -94,25 +98,56 @@ namespace TextThreadProgram
                     break;
 
                 case "Z-Order":
-                    if (outOptions.Z_Order == 0)
-                    {
-                        outOptions.Z_Order = 1;
+                    if (textProperties.Z_Order == 0)
                         propertyTextBox.Text = "Front";
-                    }
                     else
-                    {
-                        outOptions.Z_Order = 0;
                         propertyTextBox.Text = "Back";
-                    }
                     break;
+            }
+        }
+
+        private void UpdateProperties(String propName)
+        {
+            switch (propName)
+            {
+                case "Text":
+                    textProperties.StringText = comboBox1.Text;
+                    break;
+
+                case "Font":
+                    textProperties.TextFont = comboBox1.Font;
+                    break;
+                case "Text Color":
+                    textProperties.TextColor = comboBox1.BackColor;
+                    break;
+                case "Background Color":
+                    textProperties.BgColor = comboBox1.BackColor;
+                    break;
+                case "Z-Order":
+                    if (comboBox1.Text == "Front")
+                        textProperties.Z_Order = 1;
+                    else
+                        textProperties.Z_Order = 0;
+                    break;
+
             }
         }
 
         private void okayButton_Click(object sender, EventArgs e)
         {
-
+            UpdateProperties(comboBox1.Text);
 
             this.Close();
+        }
+
+        private void cancelButton_Click(object sender, EventArgs e)
+        {            
+            this.Close();
+        }
+
+        private void applyButton_Click(object sender, EventArgs e)
+        {
+            UpdateProperties(comboBox1.Text);
         }
     }
 }
